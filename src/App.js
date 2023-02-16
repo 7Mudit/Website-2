069ac0e1495c7f99f5e20fc13458b88549ae69f3
Mudit Kapoor <muditk441@gmail.com>
2023-02-16 22:22:36 +0530

    Footer try

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Navbar from './components/Navbar/Navbar';
 import './App.css';
 import Hero from './components/Hero/Hero';
-
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Hero/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,79 @@
+import React from "react";
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div>
+      <footer class="footer">
+        {/* <!--section-1--> */}
+        <div class="footer-container">
+          {/* <!--col-1--> */}
+          <div class="foo-column">
+            <h1 class="footer-heading">Get In touch</h1>
+            <p class="footer-p">Reach us for any sale & product query</p>
+            <p class="footer-p">
+              <span class="footer-span">Email:</span> info@getprintsolution.com
+            </p>
+            <p class="footer-p">
+              <span class="footer-span">Add.:</span> Longwell Ave, Westminster,
+              MD 21157, United States
+            </p>
+          </div>
+
+          {/* <!--col-2--> */}
+          <div class="foo-column">
+            <h3 class="footer-subheading">Quick Links</h3>
+            <ul class="footer-list">
+              <li class="footer-hover-item">Home</li>
+              {/* <li>
+                <span class="current">About Us</span>
+              </li> */}
+              <li class="footer-hover-item">About Us</li>
+              <li class="footer-hover-item">Shop</li>
+              <li class="footer-hover-item">Contact Us</li>
+              <li class="footer-hover-item">Login/Signup</li>
+            </ul>
+          </div>
+
+          {/* <!--col-3--> */}
+          <div class="foo-column">
+            <h3 class="footer-subheading">My Account</h3>
+            <div class="footer-list">
+              <li class="footer-hover-item">My Account</li>
+              <li class="footer-hover-item">Privacy Policy</li>
+              <li class="footer-hover-item">Refund and Return Policy</li>
+              <li class="footer-hover-item">Terms & Condition</li>
+              <li class="footer-hover-item">Checkout</li>
+            </div>
+          </div>
+
+          {/* <!--col-4--> */}
+          <div class="foo-column">
+            <p class="footer-p">Search</p>
+            <form class="form-footer">
+              <input type="text" class="form-input" />
+              <button class="footer-button">Search</button>
+            </form>
+
+            <h3 class="footer-heading">We Accept</h3>
+            <div class="footer-line">
+              <div class="footer-underline"></div>
+              <div class="footer-underline"></div>
+            </div>
+          </div>
+        </div>
+
+        {/* <!--section-2--> */}
+        <div class="footer-copyright">
+          <div class="footer-content">
+            <p class="footer-p">
+              Copyright© 2022. All Right Reserved. Owned & Managed by Get Print
+              Solutions.
+            </p>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
